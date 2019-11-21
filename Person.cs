8581396bc9ad0b788ed2b13e0dde14383f32e836
Kargin Mike <gray_cloud@mail.ru>
2019-11-21 23:39:09 +0300

    initil project version

--- Person.cs
+++ Person.cs
@@ -1,30 +1,24 @@
 ﻿namespace OverloadOperator
 {
+    /// <summary>
+    /// This class describes peoples cards entities.
+    /// </summary>
     public class Person
     {
         public int Age { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Person() { }
         public Person(string firstName, string lastNmae, int age)
         {
             Age = age;
             FirstName = firstName;
             LastName = lastNmae;
         }
-        public override string ToString()
-        {
-            return $"Name:{FirstName} {LastName}, Age: {Age}";
-        }
 
-        public override bool Equals(object obj)
-        {
-            return obj.ToString() == this.ToString();
-        }
-        public override int GetHashCode()
-        {
-            return this.ToString().GetHashCode();
-        }
+        public override string ToString() => $"Name:{FirstName} {LastName}, Age: {Age}";
+
+        public override bool Equals(object obj) => obj.ToString() == this.ToString();
+        public override int GetHashCode() => this.ToString().GetHashCode();
 
         public static bool operator ==(Person p1, Person p2) => p1.Equals(p2);
 
